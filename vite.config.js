70bc93be9b4b65f77acac2a83c6d3d6f6ee23ba5
tim der cheff <tim.hostettler@students.lerbermatt.ch>
2026-04-23 15:05:24 +0200

    config configed

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
-  base: "/dunkel-Game/"
-}
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                game: resolve(__dirname, "game.html")
+            }
+        }
+    }
+});
